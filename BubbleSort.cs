3e5621ff50469ea0bf55608be88e68648b79f782
Vineeth Vasudevan <vineeth@Vineeths-MacBook-Pro.local>
2024-05-10 21:36:14 +0530

    Implements BubbleSort aglorithm

---
+++ BubbleSort.cs
@@ -0,0 +1,22 @@
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        public static int[] Sort(int[] arr)
+        {
+            for(int i = 0; i < arr.Length -1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SortingAlgorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 25, 32, 7, 1, 10, 55, 77, 40 };
+            foreach (int i in arr)
+            {
+                System.Console.Write(i + " ");
+            }
+            BubbleSort.Sort(arr);
+            System.Console.WriteLine("\nSorted Array : ");
+            foreach (int i in arr)
+            {
+                System.Console.Write(i + " ");
+            }
+
+        }
+    }
+}
